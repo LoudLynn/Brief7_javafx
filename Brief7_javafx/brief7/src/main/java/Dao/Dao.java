5eef1f44f03b76fa643f0f53d87c02481b4e1f50
MaryamZ <99198401+LoudLynn@users.noreply.github.com>
2022-06-14 18:21:54 +0100

    main

---
+++ Brief7_javafx/brief7/src/main/java/Dao/Dao.java
@@ -0,0 +1,16 @@
+package Dao;
+
+import com.example.brief7.Student;
+import javafx.collections.ObservableList;
+
+public interface Dao {
+
+    public abstract boolean insert(Student stu);
+    //delete a candidate
+    public abstract void delete(Student stu);
+    //Update candidate name based on id
+    public abstract void update(Student stu);
+    //Get an candidate by id
+    public ObservableList<Student> getStudentList();
+    public abstract Student getStudentById(int id_cd);
+}
