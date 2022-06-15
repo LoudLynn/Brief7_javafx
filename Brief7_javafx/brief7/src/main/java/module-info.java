cc41f85d941ded81f90887af47f6e69751aef9b4
MaryamZ <99198401+LoudLynn@users.noreply.github.com>
2022-06-15 10:43:55 +0100

    Delete Brief7_javafx directory

--- Brief7_javafx/brief7/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.brief7 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.brief7 to javafx.fxml;
-    exports com.example.brief7;
-}
